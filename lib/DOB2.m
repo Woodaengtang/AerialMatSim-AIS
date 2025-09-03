2ba9488644d6f583ecb0becbf4346859877ee9d8
Woodaengtang <jhwoo200@gmail.com>
2025-09-03 20:22:07 +0900

    Refactor DOB2 observer with improved state estimation
    Refactored the DOB2 class to introduce explicit state estimation (x_hat), added a gain K for correction, and updated disturbance estimation logic. The observer now maintains and updates an estimated state vector, integrates quaternion normalization, and removes unused parabolic disturbance estimation. Added a get_state method for retrieving the estimated state.

--- lib/DOB2.m
+++ lib/DOB2.m
@@ -9,21 +9,24 @@ classdef DOB2 < handle
         Gamma_0     % Cut-off frequency of Step Disturbance (scalar gain)
         Gamma_1     % Cut-off frequency of Ramp Disturbance (scalar gain)
         Gamma_2     % Cut-off frequency of Parabolic Disturbance (scalar gain)
+        K           % Gain used to adjust the difference between the estimated state and the measured state for improved accuracy
         Dist_0      % Disturbance estimate for step input (vector, same size as disturbance 'd')
         Dist_1      % Disturbance estimate for ramp input (vector, same size as disturbance 'd')
-        Dist_2      % Disturbance estimate for parabolic input (vector, same size as disturbance 'd')
-        sys_output  % Output of f(x, u, t) + F*d_hat, representing the estimated system dynamics without true disturbance (vector, same size as state derivative)
+        x_dot
+        x_hat_dot   % Output of f(x, u, t) + F*d_hat, representing the estimated system dynamics without true disturbance (vector, same size as state derivative)
+        x_hat       % Estimated state vector of the system, representing the state after accounting for the estimated disturbances.
         d_hat       % Estimated disturbance (vector, size depends on the 'state' input to update_dist_estimate)
         F           % Transformation matrix for disturbance estimation, mapping disturbance to state space (matrix, size depends on system state and disturbance dimensions)
         pseudo_F    % Pseudo-inverse of the transformation matrix for disturbance estimation (matrix, size depends on system state and disturbance dimensions)
-        z_dot       % Product of pseudo_F * sys_output, representing the derivative of the observer state 'z' (vector, same size as 'z')
+        z_dot       % Product of pseudo_F * x_hat_dot, representing the derivative of the observer state 'z' (vector, same size as 'z')
         z           % State vector for the disturbance observer, used in the estimation process (vector, same size as 'd')
     end
 
     methods
-        function obj = DOB2(DOB2_PROPERTIES)
+        function obj = DOB2(DOB2_PROPERTIES, initCond)
             % DOB2 Constructor
-            % Initializes the DOB2 object with provided properties.
+            % This constructor initializes a new instance of the DOB2 class
+            % with the specified properties and initial conditions.
             %
             % Inputs:
             %   - DOB2_PROPERTIES: A structure containing initialization parameters.
@@ -33,28 +36,32 @@ classdef DOB2 < handle
             %                      .gamma_1: Cut-off frequency for ramp disturbance.
             %                      .gamma_2: Cut-off frequency for parabolic disturbance.
             %
+            %   - initCond: A structure containing the initial conditions for the
+            %                state of the system, including position, velocity,
+            %                quaternion, and angular velocity.
+            %
             % Output:
             %   - obj: The initialized DOB2 object.
             %
-            % Relationship between Inputs and Output:
-            %   The input structure 'DOB2_PROPERTIES' directly populates the
-            %   corresponding properties (dt, Gamma_0, Gamma_1, Gamma_2) of the
-            %   newly created 'obj' instance. Other internal states (Dist_0, Dist_1,
-            %   Dist_2, sys_output, d_hat, F, pseudo_F, z_dot) are initialized to
-            %   zero or NaN, serving as starting points for the observer's operation.
+            % The input structure 'DOB2_PROPERTIES' directly populates the
+            % corresponding properties (dt, Gamma_0, Gamma_1, Gamma_2) of the
+            % newly created 'obj' instance. Other internal states (Dist_0, Dist_1,
+            % Dist_2, x_hat_dot, d_hat, F, pseudo_F, z_dot) are initialized to
+            % zero or NaN, serving as starting points for the observer's operation.
             obj.dt = DOB2_PROPERTIES.dt;
             obj.Gamma_0 = DOB2_PROPERTIES.gamma_0;
             obj.Gamma_1 = DOB2_PROPERTIES.gamma_1;
-            obj.Gamma_2 = DOB2_PROPERTIES.gamma_2;
-            obj.Dist_0 = 0;
-            obj.Dist_1 = 0;
-            obj.Dist_2 = 0;
-            obj.sys_output = 0;
+            obj.K       = DOB2_PROPERTIES.K;
+            obj.Dist_0 = zeros([3, 1]);
+            obj.Dist_1 = zeros([3, 1]);
+            obj.x_dot = zeros([13, 1]);
+            obj.x_hat_dot = zeros([13, 1]);
+            obj.x_hat = [initCond.pos; initCond.vel; initCond.quat; initCond.omg];
             obj.d_hat = NaN; % Initialized to NaN to trigger initial zeros() assignment
             obj.F = 0;
             obj.pseudo_F = 0;
-            obj.z_dot = 0;
-            obj.z = 0; % Initializing z for the first integration step
+            obj.z_dot = zeros([3, 1]);
+            obj.z = zeros([3, 1]);
         end
 
         function obj = update_dist_estimate(obj, state, input, properties)
@@ -73,32 +80,28 @@ classdef DOB2 < handle
             %   - obj: The updated DOB2 object with new disturbance estimates.
             if isnan(obj.d_hat)
                 % Initialize d_hat and z based on the size of the state vector
-                % Assumes the disturbance acts on the same dimension as the state.
-                % This might need adjustment if disturbance dimension is different.
                 obj.d_hat = zeros([3, 1]);
-                obj.z = zeros([3, 1]); % Initialize z based on the expected size from pseudo_F * state
+                obj.z_dot = zeros([3, 1]);
+                obj.z = zeros([3, 1]);
             end
             obj.update_ode_model(state, input, properties);
             obj.integ_z();
-            obj.update_Dist_i(state);
-            obj.d_hat = obj.Gamma_0 * obj.Dist_0 + obj.Gamma_1 * obj.Dist_1 + obj.Gamma_2 * obj.Dist_2;
+            obj.update_Dist_i();
+            obj.d_hat = obj.Gamma_0 * obj.Dist_0 + obj.Gamma_1 * obj.Dist_1;
         end
 
-        function obj = update_ode_model(obj, state, input, properties)
-            % update_ode_model: Computes the time derivatives of the state variables
-            % (f(x,u,t)) and updates the disturbance observer's 'F' matrix and its
-            % pseudo-inverse 'pseudo_F'. It also calculates 'sys_output', which
-            % is the estimated system dynamics without the true disturbance, but
-            % including the effect of the *estimated* disturbance.
+        function obj = ode_model(obj, state, input, properties)
+            % ode_model: Computes the time derivatives of the state variables
+            % and updates the disturbance observer's 'F' matrix and its pseudo-inverse 'pseudo_F'.
             %
             % Inputs:
-            %   - obj: The MultiCopter object (though context suggests it's the DOB2 object).
+            %   - obj: The DOB2 object itself.
             %   - state: A vector containing the current state [pos; vel; quat; omg].
             %   - input: A vector containing the control inputs [T; Mx; My; Mz].
             %   - properties: A structure containing physical properties (mass, J, D).
             %
             % Output:
-            %   - obj: The updated DOB2 object with 'F', 'pseudo_F', and 'sys_output' updated.
+            %   - obj: The updated DOB2 object with 'x_dot', 'F', and 'pseudo_F' updated.
             
             mass = properties.mass;
             J = properties.J;
@@ -115,36 +118,44 @@ classdef DOB2 < handle
                 r,  q, -p,  0];
 
             % Rotation matrix from Body frame to Inertial frame
-            w = Quat(1); x = Quat(2); y = Quat(3); z = Quat(4);
-            rotmB2I = [w^2 + x^2 - y^2 - z^2,          2*(x*y - w*z),           2*(x*z + w*y);...
-                2*(x*y + w*z),  w^2 - x^2 + y^2 - z^2,           2*(y*z - w*x);...
-                2*(x*z - w*y),          2*(y*z + w*x), w^2 - x^2 - y^2 + z^2];
-
-            Thrust = [0; 0; input(1)];                  % Thrust is along the z-axis in body frame
-            Moment = [input(2); input(3); input(4);];   % Moments about body x, y, z axes
-
-            % 'F' matrix construction: Relates disturbance to state derivatives.
-            % Here, it's assumed disturbance primarily affects velocity (acceleration)
-            % and potentially angular velocity. The drag term is included here.
-            obj.F = [zeros([3, 3]);... 
-                     rotmB2I*D;...    
-                     zeros([7, 3])];   
-            % Note: The disturbance 'd' (which 'd_hat' estimates) is implicitly
-            % assumed to be a 3-element vector related to forces/accelerations.
-            % The exact physical meaning of 'd' depends on how 'F' is structured.
-
-            obj.pseudo_F = (obj.F'*obj.F)\obj.F';
-
-            % Nominal system dynamics (f(x, u, t)) without disturbance
+            rotmB2I = quat2rotm(Quat');
+            Thrust = [0; 0; input(1)];                                              % Thrust is along the z-axis in body frame
+            Moment = [input(2); input(3); input(4)];                                % Moments about body x, y, z axes
             dpos = Vel;                                                             % Derivative of position is velocity
             dvel = [0; 0; 9.81] - rotmB2I*Thrust./mass - rotmB2I*D*rotmB2I'*Vel;    % Acceleration (gravity, thrust, drag)
             dquat = quatOmega*Quat./2;                                              % Derivative of quaternion
             domg = J\(Moment - cross(Omg', (J*Omg)')');                             % Derivative of angular velocity (Euler's equations)
+            obj.x_dot = [dpos; dvel; dquat; domg];
+            % 'F' matrix construction: Relates disturbance to state derivatives.
+            obj.F = [zeros([3, 3]);... 
+                     eye(3);...    
+                     zeros([7, 3])];
+            
+            obj.pseudo_F = (obj.F'*obj.F)\obj.F';
+        end
 
-            % Estimated system output (f(x,u,t) + F*d_hat)
+        function obj = update_ode_model(obj, state, input, properties)
+            % update_ode_model: Computes the time derivatives of the state variables
+            % (f(x,u,t)) and updates the disturbance observer's 'F' matrix and its
+            % pseudo-inverse 'pseudo_F'. It also calculates 'x_hat_dot', which
+            % is the estimated system dynamics without the true disturbance, but
+            % including the effect of the *estimated* disturbance.
+            %
+            % Inputs:
+            %   - obj: The MultiCopter object (though context suggests it's the DOB2 object).
+            %   - state: A vector containing the current state [pos; vel; quat; omg].
+            %   - input: A vector containing the control inputs [T; Mx; My; Mz].
+            %   - properties: A structure containing physical properties (mass, J, D).
+            %
+            % Output:
+            %   - obj: The updated DOB2 object with 'F', 'pseudo_F', and 'x_hat_dot' updated.
+            
+            obj.ode_model(state, input, properties);
+
+            % Estimated system output (f(x,u,t) + F*d_hat + K(x_m - x_hat))
             % This is the system dynamics predicted by the model plus the effect
             % of the *estimated* disturbance.
-            obj.sys_output = [dpos; dvel; dquat; domg] + obj.F*obj.d_hat;
+            obj.x_hat_dot = obj.x_dot + obj.F*obj.d_hat + obj.K*(state - obj.x_hat);
         end
 
         function obj = integ_z(obj)
@@ -155,14 +166,16 @@ classdef DOB2 < handle
             %
             % Output:
             %   - obj: The updated DOB2 object with the integrated 'z' state.
-            %
-            
-            obj.z_dot = obj.pseudo_F * obj.sys_output;
+
+            obj.z_dot = obj.pseudo_F * obj.x_dot + obj.d_hat;
             obj.z = obj.z + obj.dt * obj.z_dot;
+            obj.x_hat = obj.x_hat + obj.dt * obj.x_hat_dot;
+            obj.x_hat(7:10) = obj.x_hat(7:10)./norm(obj.x_hat(7:10));
         end
 
-        function obj = update_Dist_i(obj, state)
-            % update_Dist_i: Calculates intermediate disturbance estimates (Dist_0, Dist_1, Dist_2)
+        % function obj = update_Dist_i(obj, state)
+        function obj = update_Dist_i(obj)
+            % update_Dist_i: Calculates intermediate disturba   nce estimates (Dist_0, Dist_1)
             % based on the difference between the actual system state (transformed by pseudo_F)
             % and the observer's internal state 'z'.
             %
@@ -173,9 +186,21 @@ classdef DOB2 < handle
             % Output:
             %   - obj: The updated DOB2 object with new 'Dist_0', 'Dist_1', and 'Dist_2' values.
             
-            obj.Dist_0 = obj.pseudo_F*state - obj.z;
-            obj.Dist_1 = obj.Dist_0 + obj.Dist_0*obj.dt; % This appears to be a simple integration
-            obj.Dist_2 = obj.Dist_1 + obj.Dist_1*obj.dt; % Another level of integration
+            
+            obj.Dist_0 = obj.pseudo_F*obj.x_hat - obj.z;
+            obj.Dist_1 = obj.Dist_1 + obj.Dist_0*obj.dt;
+        end
+
+
+        function state_vec = get_state(obj)
+            % get_state_quat: Retrieves the estimated state of the multi-copter.
+            %
+            % Output:
+            %   - state_vec: A vector containing the position, velocity, 
+            %     attitude (quaternion), and angular velocity of the multi-copter.
+
+            state_vec = obj.x_hat;
         end
     end
 end
+
